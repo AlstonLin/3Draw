ccadb351aeab2946e088734eb34f27c0241225b2
AlstonLin <alston@trutechinnovations.com>
2015-10-31 19:22:17 -0400

    Finished 3d Cursor Position calculator, started draw functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var scene, camera;
+var cubeCounter = 0;
+var cubeCounter = 0;
+
 window.addEventListener('DOMContentLoaded', function(){
   // get the canvas DOM element
   var canvas = document.getElementById('renderCanvas');
@@ -7,36 +11,30 @@ window.addEventListener('DOMContentLoaded', function(){
 
   // createScene function that creates and return the scene
   var createScene = function(){
-      // create a basic BJS Scene object
-      var scene = new BABYLON.Scene(engine);
+    // create a basic BJS Scene object
+    scene = new BABYLON.Scene(engine);
 
-      // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-      var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+    // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
+    camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, 10), scene);
 
-      // target the camera to scene origin
-      camera.setTarget(BABYLON.Vector3.Zero());
+    // target the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
 
-      // attach the camera to the canvas
-      camera.attachControl(canvas, false);
+    // attach the camera to the canvas
+    camera.attachControl(canvas, false);
 
-      // create a basic light, aiming 0,1,0 - meaning, to the sky
-      var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+    // create a basic light, aiming 0,1,0 - meaning, to the sky
+    var light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
 
-      // create a built-in "sphere" shape; its constructor takes 5 params: name, width, depth, subdivisions, scene
-      var sphere = new BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+    // create a built-in "sphere" shape; its constructor takes 5 params: name, width, depth, subdivisions, scene
+    var sphere = BABYLON.Mesh.CreateSphere('sphere', 16, 2, scene);
 
-      // move the sphere upward 1/2 of its height
-      sphere.position.y = 1;
-
-      // create a built-in "ground" shape; its constructor takes the same 5 params as the sphere's one
-      var ground = new BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
-
-      // return the created scene
-      return scene;
+    // return the created scene
+    return scene;
   }
 
   // call the createScene function
-  var scene = createScene();
+  scene = createScene();
 
   // run the render loop
   engine.runRenderLoop(function(){
@@ -49,10 +47,43 @@ window.addEventListener('DOMContentLoaded', function(){
   });
 });
 
+function createCube(){
+  const DEFAULT_SIZE = 1;
+  var cursorPosition = getCursorPosition();
+  var cube = BABYLON.Mesh.CreateBox('cube' + cubeCounter, DEFAULT_SIZE, scene);
+  cube.x = cursorPosition.x;
+  cube.y = cursorPosition.y;
+  cube.z = cursorPosition.z;
+  cubeCounter++;
+  return cube;
+}
+
+function createSphere(){
+  const DEFAULT_SIZE = 1;
+  var cursorPosition = getCursorPosition();
+  var cube = BABYLON.Mesh.CreateSphere('sphere' + cubeCounter, DEFAULT_SIZE, scene);
+  cube.x = cursorPosition.x;
+  cube.y = cursorPosition.y;
+  cube.z = cursorPosition.z;
+  cubeCounter++;
+  return cube;
+}
+
 function getCursorPosition(){
+  const DIR_MULTIPLIER = 3;
+
+  var cameraX = camera.position.x;
+  var cameraY = camera.position.y;
+  var cameraZ = camera.position.z;
+
+  var dirX = Math.sin(camera.rotation.x);
+  var dirY = Math.sin(camera.rotation.y);
+  var dirZ = Math.sin(camera.rotation.z);
+
   return {
-    x: 0,
-    y: 0,
-    z: 0
+    x: cameraX + DIR_MULTIPLIER * dirX,
+    y: cameraY + DIR_MULTIPLIER * dirY,
+    z: cameraZ + DIR_MULTIPLIER * dirZ
   };
 }
+
