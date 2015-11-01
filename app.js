ab4e10c7b03f40e48dfd929db99a41869c3ac835
AlstonLin <alston@trutechinnovations.com>
2015-11-01 05:47:34 -0500

    Changes url to localhost

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient
 function setupMongo(){
   var mongodb = require('mongodb');
   var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://d3draw.cloudapp.net:27017/scenes';
+  var url = 'mongodb://localhost';
   MongoClient.connect(url, function (err, db) {
     if (err) {
       console.log('Unable to connect to the mongoDB server. Error:', err);
