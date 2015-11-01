525bde612df8a0315eb4467bc53385826229a8cc
AlstonLin <alston@trutechinnovations.com>
2015-11-01 05:24:20 -0500

    Ported to Node.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var collection;
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+function setupMongo(){
+  var mongodb = require('mongodb');
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://d3draw.cloudapp.net/scenes';
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
+      collection = db.collection('scenes');
+      db.close();
+    }
+  });
+}
+setupMongo();
