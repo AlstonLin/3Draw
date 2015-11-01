4576a29b0fdab184c2fe0d2fdf89ea5efe7b0189
David Lu <david.lu@hotmail.com>
2015-11-01 01:56:07 -0500

    tried to fix id

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ var scene, camera, cursor;
 //var ObjectId = require('mongodb').ObjectId;
 //var url = 'mongodb://d3draw.cloudapp.net';
 var count = 0;
-var id;
+var id = 0;
+
+$("#submit").click(function(){
+  id = $("#id").val();
+});
 
 window.addEventListener('DOMContentLoaded', function(){
   //Increment counter
@@ -14,69 +18,79 @@ window.addEventListener('DOMContentLoaded', function(){
   // get the canvas DOM element
   var canvas = document.getElementById('renderCanvas');
 
-  // load the 3D engine
+  // load the 3D engine;
   var engine = new BABYLON.Engine(canvas, true);
 
   //Get ID
   console.log("ID: " + id);
+  
+  //Return all scenes
+  var scenes = db.collections('scenes').find().fetch();
+  //Get scene by id
+  var currentScene = db.collections('scenes').find({"_id" : id}).fetch();
+
+  if(currentScene == []) {
   // createScene function that creates and return the scene
-  var createScene = function(){
-    // create a basic BJS Scene object
-    scene = new BABYLON.Scene(engine);
+    var createScene = function(){
+      // create a basic BJS Scene object
+      scene = new BABYLON.Scene(engine);
 
-    // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-    camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, 5), scene);
+      // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
+      camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, 5), scene);
 
-    // target the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+      // target the camera to scene origin
+      camera.setTarget(BABYLON.Vector3.Zero());
 
-    // attach the camera to the canvas
-    camera.attachControl(canvas, false);
+      // attach the camera to the canvas
+      camera.attachControl(canvas, false);
 
-    // create a basic light, aiming 0,1,0 - meaning, to the sky
-    var light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
-    var materialCursor = new BABYLON.StandardMaterial("cursorTexture", scene);
-    materialCursor.diffuseColor = new BABYLON.Color3(0, 0, 0);
-    cursor = BABYLON.Mesh.CreateSphere("sphere", 10.0, 0.1, scene);
-    cursor.material = materialCursor;
+      // create a basic light, aiming 0,1,0 - meaning, to the sky
+      var light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene);
+      var materialCursor = new BABYLON.StandardMaterial("cursorTexture", scene);
+      materialCursor.diffuseColor = new BABYLON.Color3(0, 0, 0);
+      cursor = BABYLON.Mesh.CreateSphere("sphere", 10.0, 0.1, scene);
+      cursor.material = materialCursor;
 
-    // create x,y,z axis
-    var linex = new BABYLON.Mesh.CreateLines("lines1", [
-        new BABYLON.Vector3(100000, 0, 0),
-        new BABYLON.Vector3(-100000, 0, 0),
-    ], scene);
-    
-    linex.color = new BABYLON.Color3(1, 0, 0);
+      // create x,y,z axis
+      var linex = new BABYLON.Mesh.CreateLines("lines1", [
+          new BABYLON.Vector3(100000, 0, 0),
+          new BABYLON.Vector3(-100000, 0, 0),
+      ], scene);
+      
+      linex.color = new BABYLON.Color3(1, 0, 0);
 
-    var liney = new BABYLON.Mesh.CreateLines("line2", [
-        new BABYLON.Vector3(0, 100000, 0),
-        new BABYLON.Vector3(0, -100000, 0),
-    ], scene);
-    liney.color = new BABYLON.Color3(0, 1, 0);
+      var liney = new BABYLON.Mesh.CreateLines("line2", [
+          new BABYLON.Vector3(0, 100000, 0),
+          new BABYLON.Vector3(0, -100000, 0),
+      ], scene);
+      liney.color = new BABYLON.Color3(0, 1, 0);
 
-    var linez = new BABYLON.Mesh.CreateLines("line3", [
-        new BABYLON.Vector3(0, 0, 100000),
-        new BABYLON.Vector3(0, 0, -100000),
-    ], scene);
-    linez.color = new BABYLON.Color3(0, 0, 1);
+      var linez = new BABYLON.Mesh.CreateLines("line3", [
+          new BABYLON.Vector3(0, 0, 100000),
+          new BABYLON.Vector3(0, 0, -100000),
+      ], scene);
+      linez.color = new BABYLON.Color3(0, 0, 1);
 
-    // return the created scene
-    return scene;
+      // return the created scene
+      return scene;
+    }
+
+    // call the createScene function
+    scene = createScene();
+
+    //Insert scene into database
+    console.log("About to insert.");
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      insertScene(db, function() {
+        db.close();
+      }, scene);
+    });
   }
 
-  
-
-  // call the createScene function
-  scene = createScene();
-
-  //Insert scene into database
-  console.log("About to insert.");
-  /*MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    insertScene(db, function() {
-      db.close();
-    }, scene);
-  });*/
+  else { 
+    scene = currentScene;
+  }
 
   // run the render loop
   engine.runRenderLoop(function(){
@@ -117,12 +131,7 @@ window.addEventListener('keydown', function(event) {
   }
 }, false);
 
-$("#submit").click(function(){
-  id = $("#id").val();
-});
 
-
-/*
 function insertScene(db, callback, scene) {
   db.collection('scenes').insertOne({
       "scene" : scene,
@@ -135,31 +144,5 @@ function insertScene(db, callback, scene) {
     });
 }
 
-//Return all scenes
-var scenes = function getAllScenes(db, callback) {
-  var cursor = db.collection('scenes').find();
-  cursor.each(function(err, doc) {
-    assert.equal(err, null);
-    if(doc != null) {
-      console.dir(doc);
-    }
-    else {
-      callback();
-    }
-  });
-}
 
-//Get scene by id
-var scene = function getScene(db, callback, id) {
-  var cursor =db.collection('scenes').find( { "_id": id } );
-   cursor.each(function(err, doc) {
-      assert.equal(err, null);
-      if (doc != null) {
-         console.dir(doc);
-      } else {
-         callback();
-      }
-   });
 
-}
-*/
